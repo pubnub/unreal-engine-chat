9a046aab6ae5a809a7834c2d43e3f6627e91a753
KGronek-Pubnub <kamil.gronek@pubnub.com>
2024-09-30 15:22:01 +0200

    Release/v0.1.4 (#14)

--- Source/PubnubChatSDK/Private/PubnubChatSDK.cpp
+++ Source/PubnubChatSDK/Private/PubnubChatSDK.cpp
@@ -10,35 +10,10 @@
 
 void FPubnubChatSDKModule::StartupModule()
 {
-
-	FString BaseDir = IPluginManager::Get().FindPlugin("PubnubChat")->GetBaseDir();
-	FString LibraryPath;
-	
-#if PLATFORM_WINDOWS
-
-	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Source/ThirdParty/sdk/lib/Win64/pubnub-chat.dll"));
-
-#elif PLATFORM_MAC
-	LibraryPath = FPaths::Combine(*BaseDir, TEXT("Source/ThirdParty/sdk/lib/macos/libpubnub-chat.dylib"));
-
-#endif
-	
-	ChatSDKLibraryHandle = !LibraryPath.IsEmpty() ? FPlatformProcess::GetDllHandle(*LibraryPath) : nullptr;
-
-	if (!ChatSDKLibraryHandle)
-	{
-		FMessageDialog::Open(EAppMsgType::Ok, LOCTEXT("ChatSDKModuleHandle", "Failed to load pubnub third party library"));
-	}
 }
 
 void FPubnubChatSDKModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-
-	// Free the dll handle
-	FPlatformProcess::FreeDllHandle(ChatSDKLibraryHandle);
-	ChatSDKLibraryHandle = nullptr;
 }
 
 #undef LOCTEXT_NAMESPACE
