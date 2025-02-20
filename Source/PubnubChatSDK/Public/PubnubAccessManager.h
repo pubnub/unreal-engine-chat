188c36ff8a0ae22675578f1411b92b1f208ac84d
Kamil Gronek <kamil.gronek@pubnub.com>
2025-02-20 12:24:34 +0100

    Add Mac Lib, function descriptions, update uplugin with new versions.

--- Source/PubnubChatSDK/Public/PubnubAccessManager.h
+++ Source/PubnubChatSDK/Public/PubnubAccessManager.h
@@ -25,12 +25,28 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Pubnub Access Manager")
 	bool CanI(EPubnubAccessManagerPermission Permission, EPubnubAccessManagerResourceType ResourceType, FString ResourceName);
 
+	/**
+	 * Decodes an existing token and returns the object containing permissions embedded in that token.
+	 * The client can use this method for debugging to check the permissions to the resources or find out the token's ttl details.
+	 * 
+	 * @param Token Current token with embedded permissions.
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Pubnub Access Manager")
 	FString ParseToken(FString Token);
-	
+
+	/**
+	 * Update client with authentication token granted by the server.
+	 * 
+	 * @param Token Current token with embedded permissions
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Pubnub Access Manager")
 	void SetAuthToken(FString Token);
-	
+
+	/**
+	 * Update Pubnub origin used by the Chat sdk.
+	 * 
+	 * @return 0 if origin set, +1 if origin set will be applied with new connection, -1 if setting origin not enabled
+	 */
 	UFUNCTION(BlueprintCallable, Category = "Pubnub Access Manager")
 	int SetPubnubOrigin(FString Origin);
 
